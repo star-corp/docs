0fa4b36300d7bdaf629f0ffbf3b73ffafaf155bb
jro_k <67063704+jr-ok@users.noreply.github.com>
2021-02-16 14:52:24 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,184 +1,41 @@
-const React = require("react");
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import useBaseUrl from "@docusaurus/useBaseUrl";
-import Layout from "@theme/Layout";
-import GitHubLogo from "../assets/icons/github-logo.svg";
-import { useState, useEffect } from "react";
-import { CSSTransition } from "react-transition-group";
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
 
-const HomeSplash = () => {
-  const [featureWordIndex, setFeatureWordIndex] = useState(0);
-  const featureWords = ["Dynamic", "Real-Time", "High-Performance"];
-
-  const [isShow, setIsShow] = useState(true);
-
-  const changeFeatureWordIndex = (index) => {
-    setIsShow(false);
-    setFeatureWordIndex(index);
-    setIsShow(true);
-  };
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      if (featureWordIndex >= featureWords.length - 1) {
-        changeFeatureWordIndex(0);
-      } else {
-        changeFeatureWordIndex(featureWordIndex + 1);
-      }
-    }, 3600);
-    return () => clearInterval(timer);
-  }, [featureWordIndex]);
-
+function Home() {
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
   return (
-    <div className="hero home-splash">
-      <div className="container">
-        <div className="inner">
-          <div className="padding-vert--md">
-            <h1 className="title brand">Apache APISIX™</h1>
-            <h1 className="title slogan">
-              A&nbsp;
-              <span className="feature-word">
-                <CSSTransition
-                  in={isShow}
-                  timeout={2000}
-                  classNames="feature-word-text"
-                  appear={true}
-                  onEnter={(el) => {}}
-                  onEntering={(el) => {}}
-                  onEntered={(el) => {}}
-                  onExit={(el) => {}}
-                  onExiting={(el) => {}}
-                  onExited={(el) => {}}
-                >
-                  <span>{featureWords[featureWordIndex]}</span>
-                </CSSTransition>
-              </span>
-              &nbsp;Cloud-Native API Gateway
-            </h1>
-            <div className="subtitle">
-              Provides rich traffic management features such as load balancing,
-              dynamic upstream, canary release, circuit breaking,
-              authentication, observability, and more. Based on the Nginx
-              library and etcd.
-            </div>
-          </div>
-          <div className="pluginWrapper button-wrapper">
-            <Link
-              to="https://github.com/apache/apisix"
-              className="button  button--outline button--primary github"
-            >
-              <GitHubLogo className="github-logo" />
-              View on GitHub
+    <Layout
+      noFooter={true}
+      title="صفحه اصلی"
+      description="وبسایت دیتاگیت برای این راه اندازی شده که شاید کمک کوچکی باشه برای کسانی که دوست دارند برنامه نویسی رو یاد بگیرن ولی همیشه فکر میکنن که کار سختیه! توی این وبسایت به هم کمک میکنیم تا مسیر یادگیری رو برای همدیگه آسان تر کنیم."
+      keywords={['مسعود معبودی', 'Massoud Maboudi', 'دیتاگیت', 'datagit', 'datagit.ir', 'مسعود معبودی رزومه', 'رزومه مسعود معبودی', 'Massoud Maboudi profile', 'دیتا گیت', 'مسعود معبودی دیتا گیت']}>
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className={styles.hero__title}>مسعود معبودی</h1>
+          <p className={styles.hero__subtitle}>{siteConfig.tagline}</p>
+          <Link
+            className={clsx(
+              'button button--outline button--secondary button--lg',
+              styles.getStarted,
+            )}
+            to={useBaseUrl('docs/')}>
+            شروع کنیم
             </Link>
-            <Link
-              to={useBaseUrl("downloads")}
-              className="button  button--outline button--primary secondary"
-            >
-              Downloads
-            </Link>
-          </div>
         </div>
-      </div>
-    </div>
-  );
-};
-
-const LearnHow = () => (
-  <div className="hero">
-    <div className="learn-how">
-      <div className="container">
-        <div className="row">
-          <div className="col col--7">
-            <p className="hero__title">
-              <small>Description</small>
-            </p>
-            <p className="hero__subtitle">
-              <small>
-                Cloud-native microservices API gateway, delivering the ultimate
-                performance, security, open source and scalable platform for all
-                your APIs and microservices. Apache APISIX is based on Nginx and
-                etcd. Compared with traditional API gateways, APISIX has dynamic
-                routing and plug-in hot loading, which is especially suitable
-                for API management under micro-service system.
-              </small>
-            </p>
-          </div>
-          <div className="col">
-            <img
-              className="image"
-              src="https://github.com/apache/apisix/blob/master/doc/images/apisix.png?raw=true"
-              align="right"
-              alt="apisix-description"
-            />
-          </div>
+      </header>
+      <footer className={clsx('hero hero--primary', styles.heroFooter)}>
+        <div className="container">
+          <p className={styles.customHero__subtitle}>Massoud Maboudi - Datagit.ir © 2018</p>
         </div>
-      </div>
-    </div>
-  </div>
-);
-
-const Showcase = () => {
-  const { siteConfig } = useDocusaurusContext();
-  if ((siteConfig.customFields.users || []).length === 0) {
-    return null;
-  }
-  const showcase = siteConfig.customFields.users.map((user) => (
-    <a href={user.infoLink} key={user.infoLink} target="_blank">
-      <img className="user-logo" src={user.image} alt={user.caption} />
-    </a>
-  ));
-  const middleIndex = (showcase.length / 2).toFixed(0);
-
-  return (
-    <div className="hero text--center showcase">
-      <div className="container">
-        <div className="product-showcase-section">
-          <h1>Who is Using This?</h1>
-        </div>
-        <p>
-          This project is used by all these folks
-          <br />
-          Are you using this project?{" "}
-          <a
-            href="https://github.com/apache/apisix/blob/master/doc/powered-by.md"
-            target="_blank"
-            rel="noopener"
-          >
-            <u>Add your company</u>
-          </a>
-        </p>
-        <div className="user-logos">
-          <div className="logo-row">
-            <span className="user-logos-container">
-              <section>
-                <span>{showcase.slice(0, middleIndex)}</span>
-                <span>{showcase.slice(0, middleIndex)}</span>
-              </section>
-            </span>
-          </div>
-          <div className="logo-row">
-            <span className="user-logos-container">
-              <section>
-                <span>{showcase.slice(middleIndex, showcase.length)}</span>
-                <span>{showcase.slice(middleIndex, showcase.length)}</span>
-              </section>
-            </span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const Index = (props) => {
-  return (
-    <Layout>
-      <HomeSplash />
-      <LearnHow />
-      <Showcase />
+      </footer>
     </Layout>
   );
-};
+}
 
-export default Index;
+export default Home;
