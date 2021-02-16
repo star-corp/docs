72d27cf59bcc63465959ff54329a44f814eb2397
jro_k <67063704+jr-ok@users.noreply.github.com>
2021-02-16 14:46:48 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,101 +1,184 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+const React = require("react");
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import Layout from "@theme/Layout";
+import GitHubLogo from "../assets/icons/github-logo.svg";
+import { useState, useEffect } from "react";
+import { CSSTransition } from "react-transition-group";
 
-const features = [
-  {
-    title: 'Easy to Use',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
-];
+const HomeSplash = () => {
+  const [featureWordIndex, setFeatureWordIndex] = useState(0);
+  const featureWords = ["Dynamic", "Real-Time", "High-Performance"];
 
+  const [isShow, setIsShow] = useState(true);
 
+  const changeFeatureWordIndex = (index) => {
+    setIsShow(false);
+    setFeatureWordIndex(index);
+    setIsShow(true);
+  };
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (featureWordIndex >= featureWords.length - 1) {
+        changeFeatureWordIndex(0);
+      } else {
+        changeFeatureWordIndex(featureWordIndex + 1);
+      }
+    }, 3600);
+    return () => clearInterval(timer);
+  }, [featureWordIndex]);
 
-
-function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={clsx('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
-}
-
-function Home() {
-  const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
-  return (
-    <Layout
-      title={`starCo | Docs`}
-      description="Description will go into a meta tag in <head />">
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
+    <div className="hero home-splash">
+      <div className="container">
+        <div className="inner">
+          <div className="padding-vert--md">
+            <h1 className="title brand">Apache APISIX™</h1>
+            <h1 className="title slogan">
+              A&nbsp;
+              <span className="feature-word">
+                <CSSTransition
+                  in={isShow}
+                  timeout={2000}
+                  classNames="feature-word-text"
+                  appear={true}
+                  onEnter={(el) => {}}
+                  onEntering={(el) => {}}
+                  onEntered={(el) => {}}
+                  onExit={(el) => {}}
+                  onExiting={(el) => {}}
+                  onExited={(el) => {}}
+                >
+                  <span>{featureWords[featureWordIndex]}</span>
+                </CSSTransition>
+              </span>
+              &nbsp;Cloud-Native API Gateway
+            </h1>
+            <div className="subtitle">
+              Provides rich traffic management features such as load balancing,
+              dynamic upstream, canary release, circuit breaking,
+              authentication, observability, and more. Based on the Nginx
+              library and etcd.
+            </div>
+          </div>
+          <div className="pluginWrapper button-wrapper">
             <Link
-              className={clsx(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('docs/')}>
-              Get Started
+              to="https://github.com/apache/apisix"
+              className="button  button--outline button--primary github"
+            >
+              <GitHubLogo className="github-logo" />
+              View on GitHub
+            </Link>
+            <Link
+              to={useBaseUrl("downloads")}
+              className="button  button--outline button--primary secondary"
+            >
+              Downloads
             </Link>
           </div>
         </div>
-      </header>
-      <main>
-        {features && features.length > 0 && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
-              </div>
-            </div>
-          </section>
-        )}
-      </main>
+      </div>
+    </div>
+  );
+};
+
+const LearnHow = () => (
+  <div className="hero">
+    <div className="learn-how">
+      <div className="container">
+        <div className="row">
+          <div className="col col--7">
+            <p className="hero__title">
+              <small>Description</small>
+            </p>
+            <p className="hero__subtitle">
+              <small>
+                Cloud-native microservices API gateway, delivering the ultimate
+                performance, security, open source and scalable platform for all
+                your APIs and microservices. Apache APISIX is based on Nginx and
+                etcd. Compared with traditional API gateways, APISIX has dynamic
+                routing and plug-in hot loading, which is especially suitable
+                for API management under micro-service system.
+              </small>
+            </p>
+          </div>
+          <div className="col">
+            <img
+              className="image"
+              src="https://github.com/apache/apisix/blob/master/doc/images/apisix.png?raw=true"
+              align="right"
+              alt="apisix-description"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+const Showcase = () => {
+  const { siteConfig } = useDocusaurusContext();
+  if ((siteConfig.customFields.users || []).length === 0) {
+    return null;
+  }
+  const showcase = siteConfig.customFields.users.map((user) => (
+    <a href={user.infoLink} key={user.infoLink} target="_blank">
+      <img className="user-logo" src={user.image} alt={user.caption} />
+    </a>
+  ));
+  const middleIndex = (showcase.length / 2).toFixed(0);
+
+  return (
+    <div className="hero text--center showcase">
+      <div className="container">
+        <div className="product-showcase-section">
+          <h1>Who is Using This?</h1>
+        </div>
+        <p>
+          This project is used by all these folks
+          <br />
+          Are you using this project?{" "}
+          <a
+            href="https://github.com/apache/apisix/blob/master/doc/powered-by.md"
+            target="_blank"
+            rel="noopener"
+          >
+            <u>Add your company</u>
+          </a>
+        </p>
+        <div className="user-logos">
+          <div className="logo-row">
+            <span className="user-logos-container">
+              <section>
+                <span>{showcase.slice(0, middleIndex)}</span>
+                <span>{showcase.slice(0, middleIndex)}</span>
+              </section>
+            </span>
+          </div>
+          <div className="logo-row">
+            <span className="user-logos-container">
+              <section>
+                <span>{showcase.slice(middleIndex, showcase.length)}</span>
+                <span>{showcase.slice(middleIndex, showcase.length)}</span>
+              </section>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Index = (props) => {
+  return (
+    <Layout>
+      <HomeSplash />
+      <LearnHow />
+      <Showcase />
     </Layout>
   );
-}
+};
 
-export default Home;
+export default Index;
