b23c320eccca82227fe1cff94087f5d4dac18068
jro_k <67063704+jr-ok@users.noreply.github.com>
2021-02-27 16:17:35 -0500

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   someSidebar: {
     Main: ['home', 'info', 'versions'],
-    netSys: ['netsys', 'setup', 'activation', 'builds', 'netsysversions'],
+    netSys: ['netsys', 'setup', 'builds', 'netsysversions'],
+    Developer: ['devhome', 'getstarted', 'activation', 'modules', 'config'],
   },
 };
